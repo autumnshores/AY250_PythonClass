90a2d30798febcc07ad4d6bb7e2ea5f657d57476
franfmh <Francine@Francines-MacBook-Pro.local>
2013-09-18 04:24:05 -0700

    added CalCalc-1.0.tar.gz

---
+++ homework2/README.txt
@@ -0,0 +1,4 @@
+## How to install CalCalc.py
+
+1) In the CalCalc-1.0 directory, unpack CalCalc-1.0.tar.gz
+2) run in the command line: python setup.py install
---
+++ homework2/setup.py
@@ -0,0 +1,9 @@
+#Setup CalCalc
+
+import distutils
+from distutils.core import setup
+setup(name='CalCalc',
+      version='1.0',
+      author='Francine Foo',
+      author_email='francinefoo@berkeley.edu',
+      py_modules=['CalCalc'])
