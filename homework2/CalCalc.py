3eab8ff789de0f2376125bc6f2c48bb0b27cf51f
franfmh <Francine@Francines-MacBook-Pro.local>
2013-09-18 02:32:41 -0700

    modified CalCalc.py script: added argparse

--- homework2/CalCalc.py
+++ homework2/CalCalc.py
@@ -1,6 +1,15 @@
 # Calculate module
 
 import argparse
+parser = argparse.ArgumentParser(description='Calculate Stuff')
+parser.add_argument('required_arg_1', help='This positional argument is required')
+parser.add_argument('-s', action='store', dest='simple_value',
+                    help='Store a simple value')
+
+results = parser.parse_args()
+print 'required_args    =', results.required_string_1
+print 'simple_value     =', results.simple_value
+
 
 def Calculate(n):
     answer = eval(n)
