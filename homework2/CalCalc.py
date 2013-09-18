1f32593f4204a5dcd7cd0439d504e3a902432308
franfmh <Francine@Francines-MacBook-Pro.local>
2013-09-18 03:23:04 -0700

    modified CalCalc.py script: added try/except and import urllib2

--- homework2/CalCalc.py
+++ homework2/CalCalc.py
@@ -1,6 +1,5 @@
 # Calculate module
 
-import urllib2
 import argparse
 parser = argparse.ArgumentParser(description='Calculate Stuff')
 parser.add_argument('-s', action='store', dest='string',
@@ -10,8 +9,12 @@ results = parser.parse_args()
 
 
 def Calculate(n):
-    answer = eval(n)
-    print answer
+    try:
+        answer = eval(n)
+        print answer
+    except:
+        print 'Cannot evaluate using eval(). Getting Wolfram Alpha to help...'
+        import urllib2
 
 if __name__ == "__main__":
     import sys
